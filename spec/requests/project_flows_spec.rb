6b44082b403d7a67cd8548e949651c67aac7956f
dfry22 <davidfreiman@gmail.com>
2013-09-30 16:38:13 -0400

    all the projects tests pass

--- spec/requests/project_flows_spec.rb
+++ spec/requests/project_flows_spec.rb
@@ -26,6 +26,13 @@ describe "Project Listing" do
 
       page.should have_content('Project 3')
       expect(page).to have_content('Project 3')
+
+      click_link('Project 1')
+      # Expect we're on project1's show page
+      expect(current_path).to eq(project_path(project1))
+      # Expect on this page the first h1 has the text project1's title
+      page.should have_selector('h1:first', text: project1.title)
+      expect(page).to have_selector('h1:first', text: project1.title)
     end
 
     it "should display the navigation" do
@@ -41,12 +48,7 @@ describe "Project Listing" do
     	page.should have_selector(".navbar ul li.active a", text: "Projects")
     	expect(page).to have_selector(".navbar ul li.active a", text: "Projects")
 
-      click_link('Project 1')
-      # Expect we're on project1's show page
-      expect(current_path).to eq(project_path(project1))
-      # Expect on this page the first h1 has the text project1's title
-      page.should have_selector('h1:first', text: project1.title)
-      expect(page).to have_selector('h1:first', text: project1.title)
+
 
     end
   end
