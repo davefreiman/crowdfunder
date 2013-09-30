2338e47b59836210f5033174020749133e1d0cf0
dfry22 <davidfreiman@gmail.com>
2013-09-30 15:27:42 -0400

    Writing nav tests

--- spec/requests/project_flows_spec.rb
+++ spec/requests/project_flows_spec.rb
@@ -27,5 +27,20 @@ describe "Project Listing" do
       page.should have_content('Project 3')
       expect(page).to have_content('Project 3')
     end
+
+    it "should display the navigation" do
+    	visit "/"
+
+    	expect(current_path).to eq(root_path)
+    	page.should have_selector(".navbar ul li.activa a", text: "Home")
+    	expect(page).to have_selector('.navbar ul li.active a', text: "Home")
+    	
+    	page.find(".navbar ul").click_link("Projects")
+    	expect(current_path).to eq(projects_path)
+
+    	page.should have_selector(".navbar ul li.active a", text: "Projects")
+    	expect(page).to have_selector(".navbar ul li.active a", text: "Projects")
+
+    end
   end
 end
