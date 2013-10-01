264eed4760f3731f88e717294b28b8bc9da17273
dfry22 <davidfreiman@gmail.com>
2013-10-01 16:12:54 -0400

    writing more tests for navigation

--- spec/integration/project_flows_spec.rb
+++ spec/integration/project_flows_spec.rb
@@ -50,6 +50,9 @@ describe "Project Listing" do
     	page.should have_selector(".navbar ul li.active a", text: "Projects")
     	expect(page).to have_selector(".navbar ul li.active a", text: "Projects")
 
+      page.should have_selector('.navbar ul li.active a', count: 1)
+      expect(page).to have_selector('.navbar ul li.active a', count: 1)
+
       click_link 'Project 1'
       page.should have_selector('.navbar ul li.active a', text: "Projects")
       expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
