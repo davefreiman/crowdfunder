ae1ef83443bc4f5d4be101e42f546c1ecb2bc819
dfry22 <davidfreiman@gmail.com>
2013-10-01 14:03:35 -0400

    making tests for pledge models pass

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,4 +3,8 @@ class Pledge < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :project
+
+  validates :user, :presence => true
+  validates :project, :presence => true
+  validates :amount, :numericality => {:only_integer => true, :greater_than => 1}
 end
