66690c1d89605697f4de9b213a445d62881feb93
dfry22 <davidfreiman@gmail.com>
2013-10-02 13:16:15 -0400

    making tests for images pass

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
 	belongs_to :user
 	has_many :pledges
+	has_many :images, dependent: :destroy
+	
   attr_accessible :description, :goal, :teaser, :title, :user_id
 
   validates :title, presence: true, length: {maximum: 100}
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Crowdfunder::Application.routes.draw do
   resources :sessions
 
   namespace :my do 
-    resources :projects # => My::ProjectsController
+    resources :projects do# => My::ProjectsController
+      resources :images
+    end
   end
 
   match 'login' => 'sessions#new', :as => :login
