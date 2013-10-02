70da2e8e3efd5e46a9651c550f2aca7c0494b0e3
dfry22 <davidfreiman@gmail.com>
2013-10-02 15:21:42 -0400

    just crontabbed my crontab. feel it

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,8 @@ class User < ActiveRecord::Base
   validates :last_name, :presence => true
   validates :email, :presence => true
 
+  def name
+  	"#{self.(first_name)} #{self.(last_name)}"
+  end
 
 end
